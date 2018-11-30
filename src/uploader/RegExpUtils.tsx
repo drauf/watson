9f9c50e80c24becd4385369e47052e1f0b2e9d3d
Daniel Rauf <drauf@atlassian.com>
2018-11-30 15:59:26 +0100

    Parsing cpu usage files

---
+++ src/uploader/RegExpUtils.tsx
@@ -0,0 +1,30 @@
+export function getDateFromFilename(filename: string, pattern: RegExp): Date | null {
+    const epoch = matchOne(filename, pattern);
+    return new Date(parseInt(epoch) * 1000);
+}
+
+export function matchOne(str: string | undefined, pattern: RegExp): string {
+    const execArray: RegExpExecArray | null = pattern.exec(str || "");
+    if (execArray == null) {
+        console.error(`Did not match anything for string (${str}) and pattern (${pattern})`);
+        return "";
+    }
+    return execArray[1];
+}
+
+export function matchMultipleGroups(str: string | undefined, pattern: RegExp): string[] {
+    const execArray: RegExpExecArray | null = pattern.exec(str || "");
+    return execArray != null ? execArray.slice(1) : [];
+}
+
+// remember that the RegExp passed here needs to have the 'g' flag, otherwise it'll cause an infinite loop!
+export function matchMultipleTimes(str: string | undefined, pattern: RegExp): string[] {
+    const matches: string[] = [];
+
+    let execArray: RegExpExecArray | null;
+    while ((execArray = pattern.exec(str || "")) != null) {
+        matches.push(execArray[1]);
+    }
+
+    return matches;
+}
