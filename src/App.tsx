d215c416184a8fe90df44aec03ea33fcd8a7f7c3
Daniel Rauf <drauf@atlassian.com>
2018-12-06 19:38:30 +0100

    Pretty dropzone

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import * as React from 'react';
-import { Uploader } from './uploader/Uploader';
+import Dropzone from 'react-dropzone';
+import { Parser } from './parser/Parser';
+import { ThreadDump } from './types';
 
-class App extends React.Component {
-  handleFilesParsed = () => { }
+interface AppState {
+  threadDumps: ThreadDump[],
+  parser: Parser
+}
+
+class App extends React.Component<any, AppState> {
+  state: AppState = {
+    threadDumps: [],
+    parser: new Parser(this.handleFilesParsed.bind(this))
+  };
+
+  handleFilesParsed(threadDumps: ThreadDump[]) {
+    this.setState({ threadDumps: threadDumps });
+    console.log(threadDumps);
+  }
 
   render() {
     return (
-      <Uploader onFilesParsed={this.handleFilesParsed} />
+      <Dropzone
+        className="dropzone"
+        onDrop={this.state.parser.parseFiles}
+        accept=".txt"
+        multiple={true}
+      >
+        {this.state.threadDumps.length
+          ? "Uploaded and parsed \\o/"
+          : <div className="dropzone-inner">Drop files here or click to load and parse.</div>
+        }
+      </Dropzone>
     )
   }
 }
