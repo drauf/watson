9a057adc8e1acd8db82f71bd325967694ffbb712
Daniel Rauf <drauf@atlassian.com>
2019-01-10 21:19:00 +0100

    Fill navigation with content

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,12 +5,29 @@ interface NavProps {
   open: boolean;
 }
 
-const Nav: React.SFC<NavProps> = ({ open }) => {
-  const classNames = open ? "nav nav-open" : "nav";
-
-  return (
-    <div className={classNames} />
-  )
-}
+const Nav: React.SFC<NavProps> = ({ open }) => (
+  <div className={open ? "nav nav-open" : "nav"}>
+    <p className={open ? "brand" : "brand brand-hidden"}>
+      Watson
+    </p>
+    <ul>
+      <li>Summary</li>
+    </ul>
+    <ul>
+      <li>CPU Consumers</li>
+      <li>Thread Statuses</li>
+      <li>Stuck Threads</li>
+      <li>Similar Stack Traces</li>
+    </ul>
+    <ul>
+      <li>Threads Overview</li>
+      <li>Monitors</li>
+    </ul>
+    <ul>
+      <li>Flame Graph</li>
+      <li>Advanced Mode</li>
+    </ul>
+  </div >
+)
 
 export default Nav;
