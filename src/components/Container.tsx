44a696c4f48c923fb5942cafddf25fff781ae553
Daniel Rauf <drauf@atlassian.com>
2019-01-11 13:16:33 +0100

    Make navigation switch content

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,25 +4,43 @@ import Content from './Content';
 import Nav from './Nav';
 import NavToggle from './NavToggle';
 
+export enum Page {
+  Summary,
+  CpuConsumers,
+  ThreadStatuses,
+  StuckThreads,
+  SimilarStackTraces,
+  ThreadsOverview,
+  Monitors,
+  FlameGraph,
+  AdvancedMode
+}
+
 interface ContainerState {
   navigationOpen: boolean;
+  selectedPage: Page;
 }
 
 class Container extends React.Component<any, ContainerState> {
   public state: ContainerState = {
-    navigationOpen: true
-  };
+    navigationOpen: true,
+    selectedPage: Page.Summary
+  }
 
   public toggleNavigation = () => {
     this.setState((prevState) => ({ navigationOpen: !prevState.navigationOpen }))
   }
 
+  public selectPage = (selectedPage: Page) => {
+    this.setState({ selectedPage })
+  }
+
   public render() {
     return (
       <div className="container">
-        <Nav open={this.state.navigationOpen} />
+        <Nav open={this.state.navigationOpen} selectPage={this.selectPage} />
         <NavToggle open={this.state.navigationOpen} onClick={this.toggleNavigation} />
-        <Content />
+        <Content selectedPage={this.state.selectedPage} />
       </div>
     )
   }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,33 @@
 import * as React from 'react';
+import { Page } from './Container';
 
-const Content = () => <div className="content">content</div>
+interface ContentProps {
+  selectedPage: Page;
+}
+
+const Content: React.SFC<ContentProps> = ({ selectedPage }) => {
+  switch (selectedPage) {
+    case Page.Summary:
+      return <p>Summary</p>
+    case Page.CpuConsumers:
+      return <p>CpuConsumers</p>
+    case Page.ThreadStatuses:
+      return <p>ThreadStatuses</p>
+    case Page.StuckThreads:
+      return <p>StuckThreads</p>
+    case Page.SimilarStackTraces:
+      return <p>SimilarStackTraces</p>
+    case Page.ThreadsOverview:
+      return <p>ThreadsOverview</p>
+    case Page.Monitors:
+      return <p>Monitors</p>
+    case Page.FlameGraph:
+      return <p>FlameGraph</p>
+    case Page.AdvancedMode:
+      return <p>AdvancedMode</p>
+    default:
+      return <p>Oops! Something went wrong!</p>
+  }
+}
 
 export default Content;
