c3968d8ce0f92b9bda19b698b4532295caa8eec2
Daniel Rauf <drauf@atlassian.com>
2019-01-12 16:34:07 +0100

    Fix navbar scrolling bug

--- src/components/Navigation/Nav.tsx
+++ src/components/Navigation/Nav.tsx
@@ -16,26 +16,28 @@ export default class Nav extends React.Component<NavProps, any> {
   public render() {
     return (
       <div className={this.props.open ? "nav nav-open" : "nav"}>
-        <p className={this.props.open ? "brand" : "brand brand-hidden"}>
-          Watson
-        </p>
-        <ul>
-          <li onClick={this.onClick} value="0">Summary</li>
-        </ul>
-        <ul>
-          <li onClick={this.onClick} value="1">CPU Consumers</li>
-          <li onClick={this.onClick} value="2">Thread Statuses</li>
-          <li onClick={this.onClick} value="3">Stuck Threads</li>
-          <li onClick={this.onClick} value="4">Similar Stack Traces</li>
-        </ul>
-        <ul>
-          <li onClick={this.onClick} value="5">Threads Overview</li>
-          <li onClick={this.onClick} value="6">Monitors</li>
-        </ul>
-        <ul>
-          <li onClick={this.onClick} value="7">Flame Graph</li>
-          <li onClick={this.onClick} value="8">Advanced Mode</li>
-        </ul>
+        <div className="nav-content">
+          <p className={this.props.open ? "brand" : "brand brand-hidden"}>
+            Watson
+          </p>
+          <ul>
+            <li onClick={this.onClick} value="0">Summary</li>
+          </ul>
+          <ul>
+            <li onClick={this.onClick} value="1">CPU Consumers</li>
+            <li onClick={this.onClick} value="2">Thread Statuses</li>
+            <li onClick={this.onClick} value="3">Stuck Threads</li>
+            <li onClick={this.onClick} value="4">Similar Stack Traces</li>
+          </ul>
+          <ul>
+            <li onClick={this.onClick} value="5">Threads Overview</li>
+            <li onClick={this.onClick} value="6">Monitors</li>
+          </ul>
+          <ul>
+            <li onClick={this.onClick} value="7">Flame Graph</li>
+            <li onClick={this.onClick} value="8">Advanced Mode</li>
+          </ul>
+        </div>
       </div >
     )
   }
