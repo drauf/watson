7b3ab4a7ad4e5bf71c39e29096acf504e6592b5d
Daniel Rauf <drauf@atlassian.com>
2019-01-12 09:33:36 +0100

    Fix visual NavToggle bug

--- src/components/Navigation/NavToggle.tsx
+++ src/components/Navigation/NavToggle.tsx
@@ -7,7 +7,9 @@ interface NavToggleProps {
 }
 
 const NavToggle: React.SFC<NavToggleProps> = ({ open, onClick }) => (
-  <div className={open ? "nav-toggle open" : "nav-toggle"} onClick={onClick} />
+  <div className={open ? "nav-toggle open" : "nav-toggle"} onClick={onClick}>
+    <div className="nav-arrow" />
+  </div>
 )
 
 export default NavToggle;
