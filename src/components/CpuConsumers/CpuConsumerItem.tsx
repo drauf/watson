9a1a7478efc3f11e1870247ffa7bcb1deacb5a7a
Daniel Rauf <drauf@atlassian.com>
2019-01-12 13:26:19 +0100

    Add % sign on cpu consumers page

--- src/components/CpuConsumers/CpuConsumerItem.tsx
+++ src/components/CpuConsumers/CpuConsumerItem.tsx
@@ -9,7 +9,7 @@ interface CpuConsumerItemProps {
 const CpuConsumerItem: React.SFC<CpuConsumerItemProps> = ({ index, consumer }) => (
   <li key={index}>
     <h6>{consumer.calculatedValue.toFixed(2)}% - "{consumer.threadOccurences[0].name}", running for {consumer.threadOccurences[0].runningFor}</h6>
-    [ {consumer.threadOccurences.map(thread => thread.cpuUsage).join(" | ")} ]
+    [ {consumer.threadOccurences.map(thread => `${thread.cpuUsage}%`).join(" | ")} ]
   </li>
 )
 
