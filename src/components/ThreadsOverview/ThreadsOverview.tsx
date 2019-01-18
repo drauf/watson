b7b4e8fd8e87bd7812cdf4c41e2248d1ae94e48c
Daniel Rauf <drauf@atlassian.com>
2019-01-18 10:56:15 +0100

    Threads overview filters

--- src/components/ThreadsOverview/ThreadsOverview.tsx
+++ src/components/ThreadsOverview/ThreadsOverview.tsx
@@ -2,17 +2,50 @@ import React from 'react';
 import ThreadDump from '../../types/ThreadDump';
 import './ThreadsOverview.css';
 import ThreadsOverviewLegend from './ThreadsOverviewLegend';
+import ThreadsOverviewSettings from './ThreadsOverviewSettings';
 import ThreadsOverviewTable from './ThreadsOverviewTable';
 
 type ThreadsOverviewProps = {
   threadDumps: ThreadDump[];
 };
 
-const ThreadsOverview: React.SFC<ThreadsOverviewProps> = ({ threadDumps }) => (
-  < div className="threads-overview-content" >
-    <ThreadsOverviewLegend />
-    <ThreadsOverviewTable threadDumps={threadDumps} />
-  </div >
-);
+type ThreadsOverviewState = {
+  nameFilter: string;
+  stackFilter: string;
+};
 
-export default ThreadsOverview;
+export default class ThreadsOverview
+  extends React.PureComponent<ThreadsOverviewProps, ThreadsOverviewState> {
+
+  public state = {
+    nameFilter: '',
+    stackFilter: '',
+  };
+
+  public handleNameFilterChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    this.setState({ nameFilter: event.target.value });
+  }
+
+  public handleStackFilterChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    this.setState({ stackFilter: event.target.value });
+  }
+
+  public render() {
+    return (
+      <div className="threads-overview-content">
+        <ThreadsOverviewLegend />
+        <ThreadsOverviewSettings
+          nameFilter={this.state.nameFilter}
+          stackFilter={this.state.stackFilter}
+          onNameFilterChange={this.handleNameFilterChange}
+          onStackFilterChange={this.handleStackFilterChange}
+        />
+        <ThreadsOverviewTable
+          threadDumps={this.props.threadDumps}
+          nameFilter={this.state.nameFilter}
+          stackFilter={this.state.stackFilter}
+        />
+      </div>
+    );
+  }
+}
--- src/components/ThreadsOverview/ThreadsOverviewLegend.tsx
+++ src/components/ThreadsOverview/ThreadsOverviewLegend.tsx
@@ -2,26 +2,24 @@ import React from 'react';
 import './ThreadsOverview.css';
 
 const ThreadsOverviewLegend: React.SFC = () => (
-  <div className="thread-overview-legend">
-    <table>
-      <tbody>
-        <tr>
-          <td className="runnable" />
-          <td className="blocked" />
-          <td className="waiting" />
-          <td className="timed-waiting" />
-          <td className="unknown" />
-        </tr>
-        <tr>
-          <td>runnable</td>
-          <td>blocked</td>
-          <td>waiting</td>
-          <td>timed waiting</td>
-          <td>new or unknown</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <table className="thread-overview-legend">
+    <tbody>
+      <tr>
+        <td className="runnable" />
+        <td className="blocked" />
+        <td className="waiting" />
+        <td className="timed-waiting" />
+        <td className="unknown" />
+      </tr>
+      <tr>
+        <td>runnable</td>
+        <td>blocked</td>
+        <td>waiting</td>
+        <td>timed waiting</td>
+        <td>new or unknown</td>
+      </tr>
+    </tbody>
+  </table>
 );
 
 export default ThreadsOverviewLegend;
