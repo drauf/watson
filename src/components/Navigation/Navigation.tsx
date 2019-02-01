a68fc258481e61395f34e35c2c4247c0d552eae1
Daniel Rauf <drauf@atlassian.com>
2019-02-01 15:10:16 +0100

    WATSON-1: Add links to docs, source code, Jira

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -10,6 +10,12 @@ type NavigationProps = {
 
 export default class Navigation extends React.PureComponent<NavigationProps> {
 
+  private sourceCodeLink: string = 'https://bitbucket.org/atlassian/watsonjs/';
+  // tslint:disable-next-line:max-line-length
+  private issueTrackerLink: string = 'https://ecosystem.atlassian.net/secure/RapidBoard.jspa?projectKey=WAT&rapidView=501&view=planning';
+  // tslint:disable-next-line:max-line-length
+  private documentationLink: string = 'https://hello.atlassian.net/wiki/spaces/~drauf/pages/390789227/Watson+your+astute+assistant';
+
   public onClick = (page: string): React.MouseEventHandler<HTMLLIElement> => () => {
     window.scrollTo(0, 0);
     this.props.selectPage(page as Page);
@@ -37,9 +43,17 @@ export default class Navigation extends React.PureComponent<NavigationProps> {
             {/* <li onClick={this.onClick(Page.AdvancedMode)}>Advanced Mode</li> */}
           </ul>
 
-          <ul>
-            <li onClick={this.props.clearThreadDumps}>Load another thread dump</li>
-          </ul>
+          <div className="nav-content-bottom">
+            <ul>
+              <li onClick={this.props.clearThreadDumps}>Load another thread dump</li>
+            </ul>
+
+            <ul>
+              <a href={this.sourceCodeLink}><li>Source code</li></a>
+              <a href={this.issueTrackerLink}><li>Issue tracker</li></a>
+              <a href={this.documentationLink}><li>Documentation</li></a>
+            </ul>
+          </div>
         </div>
       </div >
     );
