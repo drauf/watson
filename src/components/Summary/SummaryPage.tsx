732ee2b71867863debfb9ff507fbd2d7e57f5d65
Daniel Rauf <drauf@atlassian.com>
2019-02-05 14:38:58 +0100

    WATSON-3: Show messages when no cpu usage files are loaded

--- src/components/Summary/SummaryPage.tsx
+++ src/components/Summary/SummaryPage.tsx
@@ -9,13 +9,23 @@ type SummaryPageProps = {
   threadDumps: ThreadDump[];
 };
 
-const SummaryPage: React.SFC<SummaryPageProps> = ({ threadDumps }) => (
-  <div className="content">
-    <LoadAveragesChart threadDumps={threadDumps} />
-    <MemoryUsageChart threadDumps={threadDumps} />
-    <SwapUsageChart threadDumps={threadDumps} />
-    <CurrentCpuUsageChart threadDumps={threadDumps} />
-  </div>
-);
+const SummaryPage: React.SFC<SummaryPageProps> = ({ threadDumps }) => {
+  if (threadDumps.find(dump => dump.loadAverages !== null)) {
+    return (
+      <div className="content">
+        <h2>To see the Summary you must upload at least one cpu_info file.</h2>
+      </div>
+    );
+  }
+
+  return (
+    <div className="content">
+      <LoadAveragesChart threadDumps={threadDumps} />
+      <MemoryUsageChart threadDumps={threadDumps} />
+      <SwapUsageChart threadDumps={threadDumps} />
+      <CurrentCpuUsageChart threadDumps={threadDumps} />
+    </div>
+  );
+};
 
 export default SummaryPage;
