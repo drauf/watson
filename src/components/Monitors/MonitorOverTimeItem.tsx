1c75c7e18d1c3e7b6925b9852050aa47e4d1ba6c
Daniel Rauf <drauf@atlassian.com>
2019-02-15 12:17:47 +0100

    WATSON-11: Hide monitors nobody is waiting for

--- src/components/Monitors/MonitorOverTimeItem.tsx
+++ src/components/Monitors/MonitorOverTimeItem.tsx
@@ -20,6 +20,10 @@ export default class MonitorOverTimeItem
   public render() {
     const monitor = this.props.monitor;
 
+    if (monitor.waitingSum === 0) {
+      return null;
+    }
+
     return (
       <>
         <h6 className="clickable" onClick={this.toggleCollapse}>
