8c2f9433180c08e7c8f0bca4c60f9df6bee00e08
Daniel Rauf <drauf@atlassian.com>
2019-02-15 15:26:24 +0100

    WATSON-26: Fix monitors page rendering issue in webkit

--- src/types/Thread.tsx
+++ src/types/Thread.tsx
@@ -2,20 +2,21 @@ import Lock from './Lock';
 import ThreadStatus from './ThreadStatus';
 
 export default class Thread {
-  public id!: number;
-  public name!: string;
+  public id: number;
+  public name: string;
   public date: Date | null;
   public status: ThreadStatus | undefined;
   public cpuUsage: number = 0.0;
   public runningFor!: string;
-  public lockWaitingFor!: Lock;
+  public lockWaitingFor: Lock | null = null;
   public locksHeld: Lock[] = [];
   public classicalLocksHeld: Lock[] = [];
-  public meaningfulLinesNumber!: number;
   public stackTrace: string[] = [];
   public matchingFilter: boolean = false;
 
-  constructor(date: Date | null) {
+  constructor(id: number, name: string, date: Date | null) {
+    this.id = id;
+    this.name = name;
     this.date = date;
   }
 }
