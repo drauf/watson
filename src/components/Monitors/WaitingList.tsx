d377996f519eb1236c30e3d9fb0b005a9ab6c6b2
Daniel Rauf <drauf@atlassian.com>
2019-02-15 11:55:57 +0100

    WATSON-11: Show monitors data in two columns

--- src/components/Monitors/WaitingList.tsx
+++ src/components/Monitors/WaitingList.tsx
@@ -6,10 +6,50 @@ type WaitingListProps = {
   waiting: Thread[],
 };
 
-const WaitingList: React.SFC<WaitingListProps> = ({ waiting }) => (
-  <ul className="waiting-list">
-    {waiting.map((thread, index) => <WaitingListItem thread={thread} key={index} />)}
-  </ul>
-);
+type WaitingListState = {
+  expanded: boolean;
+};
 
-export default WaitingList;
+export default class WaitingList extends React.PureComponent<WaitingListProps, WaitingListState> {
+
+  private static THREADS_TO_SHOW_WHEN_COLLAPSED = 10;
+
+  public state: WaitingListState = {
+    expanded: false,
+  };
+
+  public handleClick = () => {
+    this.setState(prevState => ({ expanded: !prevState.expanded }));
+  }
+
+  public render() {
+    if (this.props.waiting.length === 0) {
+      return null;
+    }
+
+    const collapsable = this.props.waiting.length - WaitingList.THREADS_TO_SHOW_WHEN_COLLAPSED;
+    const threads = this.state.expanded
+      ? this.props.waiting
+      : this.props.waiting.slice(0, WaitingList.THREADS_TO_SHOW_WHEN_COLLAPSED);
+
+    return (
+      <>
+        <b>{this.props.waiting.length} thread(s) waiting for notification on lock:</b>
+        <br />
+
+        {threads.map((thread, index) => <WaitingListItem thread={thread} key={index} />)}
+
+        {collapsable > 0 &&
+          <a onClick={this.toggleExpand}>
+            {this.state.expanded
+              ? `Collapse threads list (hide ${collapsable} thread(s))`
+              : `Expand threads list (${collapsable} more thread(s) to show)`}
+          </a>}
+      </>
+    );
+  }
+
+  private toggleExpand = () => {
+    this.setState(prevState => ({ expanded: !prevState.expanded }));
+  }
+}
