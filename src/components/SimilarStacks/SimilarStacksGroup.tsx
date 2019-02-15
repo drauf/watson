3471dcdcc0e4da3abf99c408c1842d432524e86c
Daniel Rauf <drauf@atlassian.com>
2019-02-15 10:34:36 +0100

    WATSON-24: Collapsing groups of similar stack traces

--- src/components/SimilarStacks/SimilarStacksGroup.tsx
+++ src/components/SimilarStacks/SimilarStacksGroup.tsx
@@ -10,14 +10,16 @@ type SimilarStacksGroupProps = {
 };
 
 type SimilarStacksGroupState = {
-  collapse: boolean;
+  collapseGroup: boolean;
+  collapseThreadsList: boolean;
 };
 
 export default class SimilarStacksGroup
   extends React.PureComponent<SimilarStacksGroupProps, SimilarStacksGroupState> {
 
   public state: SimilarStacksGroupState = {
-    collapse: true,
+    collapseGroup: false,
+    collapseThreadsList: true,
   };
 
   public render() {
@@ -25,18 +27,30 @@ export default class SimilarStacksGroup
       return null;
     }
 
+    if (this.state.collapseGroup) {
+      return (
+        <h6 className="clickable" onClick={this.toggleGroup}>
+          <span className="chevron rotate" />
+          {this.props.threadGroup.length} thread(s) with this stack:
+        </h6>
+      );
+    }
+
     const sortedByName = this.props.threadGroup.sort((t1, t2) => t1.name.localeCompare(t2.name));
     const stackTrace = this.getStackTrace(this.props.threadGroup);
-    const threads = this.state.collapse ? sortedByName.slice(0, 20) : sortedByName;
+    const threads = this.state.collapseThreadsList ? sortedByName.slice(0, 20) : sortedByName;
 
     return (
       <>
-        <h6>{this.props.threadGroup.length} thread(s) with this stack:</h6>
+        <h6 className="clickable" onClick={this.toggleGroup}>
+          <span className="chevron" />
+          {this.props.threadGroup.length} thread(s) with this stack:
+        </h6>
         <ul>
           {threads.map((thread, index) => <ThreadSummary key={index} thread={thread} />)}
           {sortedByName.length > 20 &&
-            <li><a onClick={this.toggleCollapse}>
-              {this.state.collapse
+            <li><a onClick={this.toggleCollapseThreadsList}>
+              {this.state.collapseThreadsList
                 ? `Expand threads list (${sortedByName.length - 20} more thread(s) to show)`
                 : `Collapse threads list (hide ${sortedByName.length - 20} thread(s))`}
             </a></li>}
@@ -46,8 +60,12 @@ export default class SimilarStacksGroup
     );
   }
 
-  private toggleCollapse = () => {
-    this.setState(prevState => ({ collapse: !prevState.collapse }));
+  private toggleGroup = () => {
+    this.setState(prevState => ({ collapseGroup: !prevState.collapseGroup }));
+  }
+
+  private toggleCollapseThreadsList = () => {
+    this.setState(prevState => ({ collapseThreadsList: !prevState.collapseThreadsList }));
   }
 
   private getStackTrace = (threads: Thread[]): string[] => {
