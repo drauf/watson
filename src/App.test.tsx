20e0d5be39305f0c7a6e5fba7523c73b748d8606
Daniel Rauf <drauf@atlassian.com>
2019-03-28 11:25:56 +0100

    WATSON-44: Fix tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MemoryRouter as Router, Route, withRouter } from 'react-router-dom';
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  ReactDOM.render(
+    <Router>
+      <Route component={withRouter(App)} />
+    </Router>,
+    div,
+  );
+
   ReactDOM.unmountComponentAtNode(div);
 });
