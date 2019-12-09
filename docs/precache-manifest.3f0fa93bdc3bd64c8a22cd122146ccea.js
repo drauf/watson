44aa353457fe746dd293d4c33c2d60239469f252
Daniel Rauf <drauf@atlassian.com>
2019-12-09 11:31:21 +0100

    NONE: Add docs folder for github pages

---
+++ docs/precache-manifest.3f0fa93bdc3bd64c8a22cd122146ccea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd5b03fba10add0577cc",
+    "url": "/static/css/main.6e4c32b5.chunk.css"
+  },
+  {
+    "revision": "cd5b03fba10add0577cc",
+    "url": "/static/js/main.8689c0c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dca295b731ce2c38da70",
+    "url": "/static/js/2.9d5e4b4a.chunk.js"
+  },
+  {
+    "revision": "13f605a1623f6763db1a961d7f2ba6f1",
+    "url": "/index.html"
+  }
+];
