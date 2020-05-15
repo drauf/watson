471c7b165ecf79929fa0674a202bb248853321d1
Daniel Rauf <drauf@atlassian.com>
2020-05-15 15:53:03 +0200

    Updates

---
+++ precache-manifest.6da9ac1e1539efe413e7476a0cfd875f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392999339ca279c84c4bca39d4b871a3",
+    "url": "https://drauf.github.io/watson/index.html"
+  },
+  {
+    "revision": "6e9a512dbc29ffcb5518",
+    "url": "https://drauf.github.io/watson/static/css/main.e8a52af6.chunk.css"
+  },
+  {
+    "revision": "9a5d09645f50a13edb47",
+    "url": "https://drauf.github.io/watson/static/js/2.bf7a9446.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "https://drauf.github.io/watson/static/js/2.bf7a9446.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9a512dbc29ffcb5518",
+    "url": "https://drauf.github.io/watson/static/js/main.ec12784d.chunk.js"
+  },
+  {
+    "revision": "2fdb4e705a437b886eec",
+    "url": "https://drauf.github.io/watson/static/js/runtime-main.c4bce0f0.js"
+  },
+  {
+    "revision": "ecb8f00bcf0c800563db60036b304fe1",
+    "url": "https://drauf.github.io/watson/static/media/chevron.ecb8f00b.svg"
+  }
+]);
