33a5af0042ca773248d8cc6b9c8f88cc6ba0401c
Daniel Rauf <drauf@atlassian.com>
2020-08-06 16:28:43 +0200

    Prettify thread details window
    closes #25

--- src/components/ThreadDetails/ThreadDetailsWindow.tsx
+++ src/components/ThreadDetails/ThreadDetailsWindow.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Thread from '../../types/Thread';
-import ThreadStatus from '../../types/ThreadStatus';
 import './ThreadDetailsWindow.css';
 import WindowPortal from './WindowPortal';
 
@@ -11,63 +10,114 @@ type Props = {
 
 const ThreadDetailsWindow: React.SFC<Props> = ({ thread, onUnload }) => (
   <WindowPortal windowTitle={thread.name} className="thread-details" onUnload={onUnload}>
-    {renderName(thread)}
-    {renderStatus(thread)}
-    {renderLockWaitingFor(thread)}
-    {renderLocksHeld(thread)}
-    {renderStackTrace(thread)}
+    <div className="details-header">
+      {renderName(thread)}
+      {renderStatus(thread)}
+      {renderCpuUsage(thread)}
+      {renderRunningFor(thread)}
+    </div>
+    <div className="details-body">
+      {renderWaitingFor(thread)}
+      {renderLocksHeld(thread)}
+      {renderStackTrace(thread)}
+    </div>
   </WindowPortal>
 );
 
 const renderName = (thread: Thread) => {
   return (
-    <h3>{Thread.getFormattedTime(thread)} <i>{thread.name}</i></h3>
+    <h3>{Thread.getFormattedTime(thread)} - <i>{thread.name}</i></h3>
   );
 };
 
 const renderStatus = (thread: Thread) => {
   if (thread.status === undefined) {
-    return null;
+    return <div>State: <span className="thread-state unknown">UNKNOWN STATE</span></div>;
   }
 
   return (
-    <span>
-      <b>
+    <div>
+      State:
+      <span className={`thread-state ${thread.status}`}>
         {thread.status.toLocaleUpperCase()}
-        {thread.status === ThreadStatus.RUNNABLE &&
-          <> ({thread.cpuUsage.toFixed(1)}% CPU usage)</>}
-      </b>
-      , running for: {thread.runningFor}
-    </span>
+      </span>
+    </div>
+  );
+};
+
+const renderRunningFor = (thread: Thread) => {
+  return <div>Running for: {thread.runningFor}</div>;
+};
+
+const renderCpuUsage = (thread: Thread) => {
+  return (
+    <div>
+      CPU usage:
+      <span className={`thread-state ${getCpuUsageClassName(thread.cpuUsage)}`}>
+        {thread.cpuUsage.toFixed(1)}%
+          </span>
+    </div>
+  );
+};
+
+const getCpuUsageClassName = (cpuUsage: number): string => {
+  // The numbers here are completely arbitrary
+  if (cpuUsage > 78) {
+    return 'high';
+  }
+  if (cpuUsage > 42) {
+    return 'medium';
+  }
+  if (cpuUsage > 10) {
+    return 'low';
+  }
+  return 'none';
+};
+
+const renderWaitingFor = (thread: Thread) => {
+  return (
+    <div>
+      <h5>Waiting for</h5>
+
+      {!thread.lockWaitingFor &&
+        <span>This thread is not waiting for notification on any lock</span>}
+
+      {thread.lockWaitingFor &&
+        <span>
+          This thread is waiting for notification on lock [{thread.lockWaitingFor.id}]
+          {thread.lockWaitingFor.owner
+            ? <> owned by <i>{thread.lockWaitingFor.owner.name}</i></>
+            : <> without an owner</>}
+        </span>}
+    </div>
   );
 };
 
 const renderLocksHeld = (thread: Thread) => {
   return (
-    thread.locksHeld.length > 0 &&
-    <span><b>Locks held:</b> [{thread.locksHeld.map(lock => lock.id).join(', ')}]</span>
-  );
-};
+    <div>
+      <h5>Locks held</h5>
 
-const renderLockWaitingFor = (thread: Thread) => {
-  return (
-    thread.lockWaitingFor &&
-    <span>
-      <b>Waiting for notification on lock:</b> [{thread.lockWaitingFor.id}]
-        {thread.lockWaitingFor.owner
-        ? <> owned by <i>{thread.lockWaitingFor.owner.name}</i></>
-        : <> without an owner</>}
-    </span>
+      {thread.locksHeld.length === 0 &&
+        <span>This thread does not hold any locks</span>}
+
+      {thread.locksHeld.length > 0 &&
+        <span>This thread holds [{thread.locksHeld.map(lock => lock.id).join(', ')}]</span>}
+    </div>
   );
 };
 
 const renderStackTrace = (thread: Thread) => {
   return (
-    <textarea
-      wrap="off"
-      readOnly={true}
-      className="stacktrace-window"
-      value={thread.stackTrace.join('\n')} />
+    <div className="stacktrace-container">
+      <h5>Stack trace</h5>
+
+      <textarea
+        wrap="off"
+        readOnly={true}
+        className="stacktrace-textarea"
+        value={thread.stackTrace.join('\n')} />
+    </div>
   );
 };
 
