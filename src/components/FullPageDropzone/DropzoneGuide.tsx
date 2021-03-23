effdd154d2dbe7a5c2fd1caa6a41a8c39dfd2da3
Daniel Rauf <danielrauf11@gmail.com>
2021-03-23 11:55:46 +0100

    Refactor dropzone to PureComponents

--- src/components/FullPageDropzone/DropzoneGuide.tsx
+++ src/components/FullPageDropzone/DropzoneGuide.tsx
@@ -2,42 +2,49 @@ import React from 'react';
 import { ISSUE_TRACKER_LINK, SOURCE_CODE_LINK } from '../Navigation/Navigation';
 import OutboundLink from '../Navigation/OutboundLink';
 
-const header = 'For the full experience, you should gather thread dumps along with <i>top</i> outputs.';
-const GATHER_DATA_LINK = 'https://github.com/drauf/watson/blob/master/README.md#gathering-thread-dumps';
-const disclaimer = 'Watson works fully offline. No files will leave your machine.';
+export default class DropzoneGuide extends React.PureComponent {
+  private static GATHER_DATA_LINK = 'https://github.com/drauf/watson/blob/master/README.md#gathering-thread-dumps';
 
-const stopPropagation = (event: React.MouseEvent) => {
-  // prevent opening the dropzone popup when clicking on the navigation links
-  event.stopPropagation();
-};
+  private stopPropagation = (event: React.MouseEvent) => {
+    // prevent opening the dropzone popup when clicking on the navigation links
+    event.stopPropagation();
+  };
 
-const DropzoneGuide: React.FunctionComponent = () => (
-  // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions
-  <div id="dropzone-guide" role="complementary" onClick={stopPropagation}>
-    <h5 dangerouslySetInnerHTML={{ __html: header }} />
-    <span>
-      See:
-      {' '}
-      <OutboundLink to={GATHER_DATA_LINK}>
-        How to gather data
-      </OutboundLink>
-    </span>
+  public render(): JSX.Element {
+    return (
+      <div id="dropzone-guide" role="complementary" onClick={this.stopPropagation}>
+        <h5>
+          For the full experience, you should gather thread dumps along with
+          {' '}
+          <i>top</i>
+          {' '}
+          outputs.
+        </h5>
+        <span>
+          See:
+          {' '}
+          <OutboundLink to={DropzoneGuide.GATHER_DATA_LINK}>
+            How to gather data
+          </OutboundLink>
+        </span>
 
-    <p dangerouslySetInnerHTML={{ __html: disclaimer }} />
+        <p>
+          Watson works fully offline. No files will leave your machine.
+        </p>
 
-    <ul id="dropzone-links">
-      <li>
-        <OutboundLink to={ISSUE_TRACKER_LINK}>
-          Issue tracker
-        </OutboundLink>
-      </li>
-      <li>
-        <OutboundLink to={SOURCE_CODE_LINK}>
-          Source code
-        </OutboundLink>
-      </li>
-    </ul>
-  </div>
-);
-
-export default DropzoneGuide;
+        <ul id="dropzone-links">
+          <li>
+            <OutboundLink to={ISSUE_TRACKER_LINK}>
+              Issue tracker
+            </OutboundLink>
+          </li>
+          <li>
+            <OutboundLink to={SOURCE_CODE_LINK}>
+              Source code
+            </OutboundLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
--- src/components/FullPageDropzone/FullPageDropzone.tsx
+++ src/components/FullPageDropzone/FullPageDropzone.tsx
@@ -8,24 +8,6 @@ import DropzoneGuide from './DropzoneGuide';
 import './FullPageDropzone.css';
 
 class FullPageDropzone extends React.PureComponent<RouteComponentProps> {
-  public render = () => (
-    /* eslint-disable react/jsx-props-no-spreading */
-    <Dropzone multiple onDrop={this.onDrop}>
-      {({ getRootProps, getInputProps, isDragActive }) => (
-        <div id="dropzone" {...getRootProps()}>
-          <input {...getInputProps()} />
-          {
-            isDragActive
-              ? <h4>Drop files here...</h4>
-              : <h4>Drop the catalog here, or click to select files to load.</h4>
-          }
-          <DropzoneGuide />
-        </div>
-      )}
-    </Dropzone>
-    /* eslint-enable react/jsx-props-no-spreading */
-  );
-
   private onDrop = (files: File[]): void => {
     const parser = new Parser(this.onParsed);
     parser.parseFiles(files);
@@ -41,6 +23,26 @@ class FullPageDropzone extends React.PureComponent<RouteComponentProps> {
       history.push(`/${key}/similar-stacks/`);
     }
   };
+
+  public render(): JSX.Element {
+    return (
+      /* eslint-disable react/jsx-props-no-spreading */
+      <Dropzone multiple onDrop={this.onDrop}>
+        {({ getRootProps, getInputProps, isDragActive }) => (
+          <div id="dropzone" {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+              isDragActive
+                ? <h4>Drop files here...</h4>
+                : <h4>Drop the catalog here, or click to select files to load.</h4>
+            }
+            <DropzoneGuide />
+          </div>
+        )}
+      </Dropzone>
+      /* eslint-enable react/jsx-props-no-spreading */
+    );
+  }
 }
 
 export default withRouter(FullPageDropzone);
