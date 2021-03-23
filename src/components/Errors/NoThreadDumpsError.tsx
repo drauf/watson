2eda4d88158f8017b92ddeff0e6f052426109422
Daniel Rauf <danielrauf11@gmail.com>
2021-03-23 11:46:02 +0100

    Refactor errors and fillters to PureComponents

--- src/components/Errors/NoThreadDumpsError.tsx
+++ src/components/Errors/NoThreadDumpsError.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const NoThreadDumpsError: React.FunctionComponent = () => (
-  <main id="error">
-    <h4>
-      You need to load at least one file containing
-      {' '}
-      <i>thread dumps</i>
-      {' '}
-      to see this data.
-    </h4>
-  </main>
-);
-
-export default NoThreadDumpsError;
+export default class NoThreadDumpsError extends React.PureComponent {
+  public render(): JSX.Element {
+    return (
+      <main id="error">
+        <h4>
+          You need to load at least one file containing
+          {' '}
+          <i>thread dumps</i>
+          {' '}
+          to see this data.
+        </h4>
+      </main>
+    );
+  }
+}
