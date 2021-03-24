ac58326ee32b7bb47a6d18cdb0499eccac0905b1
Daniel Rauf <danielrauf11@gmail.com>
2021-03-24 12:16:39 +0100

    Change #error to #centered

--- src/components/Errors/PageNotFoundError.tsx
+++ src/components/Errors/PageNotFoundError.tsx
@@ -4,7 +4,7 @@ import { WithThreadDumpsProps } from '../../common/withThreadDumps';
 export default class PageNotFoundError extends React.PureComponent<WithThreadDumpsProps> {
   public render(): JSX.Element {
     return (
-      <main id="error">
+      <main id="centered">
         <h4>Oops, you&apos;ve found a dead link!</h4>
       </main>
     );
