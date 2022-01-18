2d6688c4cfaf20dc2dc6672b9f160326c5b2c8f7
Daniel Rauf <danielrauf11@gmail.com>
2022-01-18 10:21:38 +0100

    Bump more eslint dependencies

--- src/components/PageWithSettings.tsx
+++ src/components/PageWithSettings.tsx
@@ -10,20 +10,20 @@ export default class PageWithSettings<S> extends React.PureComponent<WithThreadD
     const { name } = event.target;
     const isChecked: boolean = event.target.checked;
     const newState: ComponentState = { [name]: isChecked };
-    this.setState(newState);
+    this.setState(newState as S);
   };
 
   protected handleIntegerChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const { name } = event.target;
     const value: number = parseInt(event.target.value ? event.target.value : '0', 10);
     const newState: ComponentState = { [name]: value > 0 ? value : 0 };
-    this.setState(newState);
+    this.setState(newState as S);
   };
 
   protected handleTextChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     const { name } = event.target;
     const { value } = event.target;
     const newState: ComponentState = { [name]: value };
-    this.setState(newState);
+    this.setState(newState as S);
   };
 }
