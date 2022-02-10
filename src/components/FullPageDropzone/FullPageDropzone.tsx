257a5378e764e955f394229190c76fb0519b2364
Daniel Rauf <danielrauf11@gmail.com>
2022-02-10 10:59:40 +0100

    Include more info on the main page

--- src/components/FullPageDropzone/FullPageDropzone.tsx
+++ src/components/FullPageDropzone/FullPageDropzone.tsx
@@ -26,21 +26,30 @@ class FullPageDropzone extends React.PureComponent<RouteComponentProps> {
 
   public render(): JSX.Element {
     return (
-      /* eslint-disable react/jsx-props-no-spreading */
-      <Dropzone multiple onDrop={this.onDrop}>
-        {({ getRootProps, getInputProps, isDragActive }) => (
-          <div id="dropzone" {...getRootProps()}>
-            <input {...getInputProps()} />
-            {
-              isDragActive
-                ? <h4>Drop files here...</h4>
-                : <h4>Drop the catalog here, or click to select files to load.</h4>
-            }
-            <DropzoneGuide />
-          </div>
-        )}
-      </Dropzone>
-      /* eslint-enable react/jsx-props-no-spreading */
+      <>
+        <div className="announcement">
+          <span className="ellipsis">
+            Watson is a JVM thread dump and CPU usage analyzer.
+            It combines the best features of other Java TDAs and optionally hides a lot of noise, like idle threads.
+          </span>
+        </div>
+
+        <Dropzone multiple onDrop={this.onDrop}>
+          {({ getRootProps, getInputProps, isDragActive }) => (
+            /* eslint-disable react/jsx-props-no-spreading */
+            <div id="dropzone" {...getRootProps()}>
+              <input {...getInputProps()} />
+              {
+                isDragActive
+                  ? <h4>Drop files here...</h4>
+                  : <h4>Drop a catalog here, or click to select files to load.</h4>
+              }
+              <DropzoneGuide />
+            </div>
+            /* eslint-enable react/jsx-props-no-spreading */
+          )}
+        </Dropzone>
+      </>
     );
   }
 }
