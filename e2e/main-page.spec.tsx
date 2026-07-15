2a7fd176fe0d9890893838126a950fa3ad47fc35
Daniel Rauf <drauf@atlassian.com>
2026-07-15 16:50:54 +0200

    Fix linter problems in e2e tests

--- e2e/main-page.spec.tsx
+++ e2e/main-page.spec.tsx
@@ -1,14 +1,14 @@
-import {expect} from '@playwright/test';
-import {test} from './e2e-common';
+import { expect } from '@playwright/test';
+import { test } from './e2e-common';
 
 test.describe('Main page', () => {
-  test('loads', async ({page}) => {
+  test('loads', async ({ page }) => {
     await page.goto('/');
 
     await expect(page).toHaveScreenshot();
   });
 
-  test('redirects after uploading files', async ({pageWithData}) => {
+  test('redirects after uploading files', async ({ pageWithData }) => {
     await expect(pageWithData).toHaveScreenshot();
   });
 });
--- e2e/stuck-threads.spec.tsx
+++ e2e/stuck-threads.spec.tsx
@@ -1,15 +1,15 @@
-import {expect} from '@playwright/test';
-import {test} from './e2e-common';
+import { expect } from '@playwright/test';
+import { test } from './e2e-common';
 
 test.describe('Stuck threads', () => {
   const MIN_STACKS = 'Detection threshold';
   const MAX_LINES = 'Similarity tolerance';
 
-  test.beforeEach(async ({pageWithData}) => {
+  test.beforeEach(async ({ pageWithData }) => {
     await pageWithData.getByText('Stuck threads').click();
   });
 
-  test('loads', async ({pageWithData}) => {
+  test('loads', async ({ pageWithData }) => {
     expect(await pageWithData.getByText('Active').isChecked()).toBeTruthy();
     expect(await pageWithData.getByLabel(MIN_STACKS).inputValue()).toBe('10');
     expect(await pageWithData.getByLabel(MAX_LINES).inputValue()).toBe('5');
@@ -17,41 +17,41 @@ test.describe('Stuck threads', () => {
     await expect(pageWithData).toHaveScreenshot();
   });
 
-  test('has working filters', async ({pageWithData}) => {
+  test('has working filters', async ({ pageWithData }) => {
     await pageWithData.getByLabel(MIN_STACKS).fill('5');
     await pageWithData.getByLabel(MAX_LINES).fill('30');
 
     await expect(pageWithData).toHaveScreenshot();
   });
 
-  test('shows empty state', async ({pageWithData}) => {
+  test('shows empty state', async ({ pageWithData }) => {
     await pageWithData.getByLabel(MIN_STACKS).fill('2137');
 
     await expect(pageWithData).toHaveScreenshot();
   });
 
-  test('can fold sections', async ({pageWithData}) => {
+  test('can fold sections', async ({ pageWithData }) => {
     await pageWithData.getByText('Active').uncheck();
     const buttons = (await pageWithData.locator('main').getByRole('button').all()).slice(0, 10);
 
     for (const button of buttons) {
-      await button.click()
+      await button.click();
     }
 
     await expect(pageWithData).toHaveScreenshot();
   });
 
-  test('has working regex filters', async ({pageWithData}) => {
+  test('has working regex filters', async ({ pageWithData }) => {
     await pageWithData.getByPlaceholder('e.g. http.*exec').fill('caesium');
     await pageWithData.getByPlaceholder('e.g. java\\.io').fill('CountDownLatch');
 
     await expect(pageWithData).toHaveScreenshot();
   });
 
-  test('opens thread details', async ({context, pageWithData}) => {
+  test('opens thread details', async ({ context, pageWithData }) => {
     const [details] = await Promise.all([
       context.waitForEvent('page'),
-      pageWithData.locator('.group-details').getByRole('button').first().click()
+      pageWithData.locator('.group-details').getByRole('button').first().click(),
     ]);
 
     await expect(details).toHaveScreenshot();
