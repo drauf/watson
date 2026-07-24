e69634f4deaed8b6e4d93bb0d085b5830749913d
Daniel Rauf <drauf@atlassian.com>
2026-07-24 15:31:03 +0200

    Make navbar sticky
    closes ##36

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,20 @@
-import { Outlet, useLoaderData, useParams } from 'react-router-dom';
+import {
+  Outlet, useLoaderData, useLocation, useParams,
+} from 'react-router-dom';
+import { useEffect } from 'react';
 import { WithThreadDumpsProps } from '../common/withThreadDumps';
 import { TimeWindowProvider } from '../context/TimeWindowContext';
 import './Container.css';
 import Navigation from './Navigation/Navigation';
 
 const Container = () => {
+  const { pathname } = useLocation();
   const { threadDumps } = useLoaderData() as WithThreadDumpsProps;
   const { threadDumpsHash } = useParams();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
   if (!threadDumpsHash) {
     throw new Error('threadDumpsHash is undefined');
